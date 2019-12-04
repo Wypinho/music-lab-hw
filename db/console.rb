a20ae2ea7319bf4f3af535fb1e4b4b1a36b3f270
Andrew Wyper <andy_wyper@yahoo.co.uk>
2019-12-04 16:10:13 +0000

    created album class, instantiated and saved

--- db/console.rb
+++ db/console.rb
@@ -2,6 +2,8 @@ require('pry')
 require_relative('../models/artist')
 require_relative('../models/album')
 
+Artist.delete_all()
+
 artist1 = Artist.new ({
   'name' => "Metallica"
   })
@@ -11,6 +13,13 @@ artist2 = Artist.new ({
   'name' => "Paul Simon"
   })
 artist2.save()
+
+album1 = Album.new ({
+  'title' => "Master of Puppets",
+  'genre' => "Metal",
+  'artist_id' => artist1.id
+  })
+album1.save()
 
 binding.pry
 nil
--- models/artist.rb
+++ models/artist.rb
@@ -3,6 +3,8 @@ require_relative('../db/sql_runner')
 
 class Artist
 
+  attr_reader :id, :name
+
   def initialize( options )
     @id = options['id'].to_i if options['id']
     @name = options['name']
@@ -14,6 +16,11 @@ class Artist
     ) VALUES ($1) RETURNING id;"
     values = [@name]
     @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM artists;"
+    SqlRunner.run(sql)
   end
 
 end
